0f53ee1671d9f750d79b413fb7c52b1d23e90050
NicolBolis <cyo@192.168.1.8>
2015-12-01 21:04:31 -0500

    Now launches missiles as soon as you are on the same level. Has a small bug where two missiles launch at nearly the same time.

--- src/cyo/Alien.java
+++ src/cyo/Alien.java
@@ -8,6 +8,7 @@ public class Alien extends GameObject {
 	private Image stillImage;
 	private int launchTime = 0;
 	private int launchNeed = 20; 
+	private boolean stopping = false;
 
 	public Alien(Board inBoard, double startX, double startY) {
 		board = inBoard;
@@ -16,24 +17,46 @@ public class Alien extends GameObject {
 		x = startX;
 		y = startY;
 		myImage = stillImage;
-		
+
 	}
 	public void update(int timeSenceLast) {
 
-
 		if (board.locatePlayer().y == this.y) {
-			if (launchTime == launchNeed) {
-				launchTime = 0;
+
+			if (stopping == false) {
+
 				if (board.locatePlayer().x < this.x) {
 					Missile boom = new Missile(board, this.x, this.y, true);
 					board.addGameObject(boom);
+
+					stopping = true;
 				} else {
 					Missile boom = new Missile(board, this.x, this.y, false);
 					board.addGameObject(boom);
+
+					stopping = true;
 				}
 			} else {
-				launchTime++;
+
+				if (launchTime == launchNeed) {
+					launchTime = 0;
+					if (board.locatePlayer().x < this.x) {
+						Missile boom = new Missile(board, this.x, this.y, true);
+						board.addGameObject(boom);
+					} else {
+						Missile boom = new Missile(board, this.x, this.y, false);
+						board.addGameObject(boom);
+
+					}
+				} else {
+					launchTime++;
+
+				}
+
 			}
+		}
+		if (Math.abs(board.locatePlayer().y - this.y) > 4) {
+			stopping = false;
 		}
 	}
 }
